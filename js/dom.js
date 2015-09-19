0ddabbd6b8926a4240c6804d3d6e1e822e117493
taranir <shikonarella@gmail.com>
2015-09-19 18:08:18 -0400

    dom injection done, still need to figure out detecting page change

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,19 @@
 console.log("dom!");
+
+(function() {
+	var hashtagList = ["dog", "cute", "adorbs", "cutenessoverload"];
+	var hashtagButton = $("<div/>");
+	hashtagButton
+		.addClass("hashtag-button")
+		.css("background-image", "url(" + chrome.extension.getURL("icon.png") + ")")
+		.attr("title", "Generate hashtags!");
+
+	hashtagButton.on('click', function(e) {
+		console.log("clicked");
+		$(e.target).parent()
+			.find(".-cx-PRIVATE-PostInfo__commentCreatorInput")
+			.val("stuff");
+	});
+
+	$(".-cx-PRIVATE-PostInfo__likeButton").after(hashtagButton);
+})();
